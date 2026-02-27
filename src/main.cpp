058a655b7ccca8925ba393dc528a4272f40c6120
Florian TheHoe <216393018+FloTheHoe@users.noreply.github.com>
2026-02-27 18:51:44 +0100

    Refactor loop to add LED blinking sequence
    Modified loop to include blinking logic for LEDs.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,27 @@
 #include <Arduino.h>
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   Serial.println("Hello, World!");
-  delay(5000);
   digitalWrite(7, LOW);
   digitalWrite(8, HIGH);
+  
+  delay(3000);
+
+  for (int i = 0; i < 3; i++)
+  {
+    digitalWrite(7, LOW);
+    digitalWrite(8, HIGH);
+    delay(500);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, LOW);
+    delay(500);
+  }
 }
